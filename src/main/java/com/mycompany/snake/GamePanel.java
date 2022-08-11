5724141b7f0e65569ec7c93bb9ad8ea416b1d5c7
gomezcamilo9701 <gomezcamilo9701@gmail.com>
2022-08-11 09:51:00 -0500

    Uploading basics Java classes as GameFrame, GamePanel and Snake

---
+++ src/main/java/com/mycompany/snake/GamePanel.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.snake;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import java.util.Random;
+
+/**
+ *
+ * @author camilo
+ */
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel () {
+        
+    }
+    
+    public void startGame () {
+        
+    }
+    
+    public void paintComponent (Graphics g) {
+        
+    }
+    
+    public void draw (Graphics g) {
+        
+    }
+    
+    public void move () {
+        
+    }
+    
+    public void checkApple () {
+        
+    }
+    
+    public void checkCollisions () {
+        
+    }
+    
+    public void gameOver (Graphics g) {
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+    
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed (KeyEvent e) {
+            
+        }
+    }
+    
+}
---
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.snake;
+
+/**
+ *
+ * @author camilo
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+         new GameFrame();
+    }
+}
